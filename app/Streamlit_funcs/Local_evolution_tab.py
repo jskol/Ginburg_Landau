3c83f963b189d0d7b97b60e3f9f6da0ea0bf52d8
jskol <skolimowski.jan@gmail.com>
2026-01-30 11:33:52 +0100

    adding dash lib

--- app/Streamlit_funcs/Local_evolution_tab.py
+++ app/Streamlit_funcs/Local_evolution_tab.py
@@ -1,4 +1,7 @@
-
+from dash 
 
 def display_time_evolution_local():
+    #pick a site to showcase
+    site=st.slider
+    
     pass
--- app/Streamlit_funcs/sidebar_content.py
+++ app/Streamlit_funcs/sidebar_content.py
@@ -26,13 +26,23 @@ def sidebar_content(params: dict[str,float|str]):
         params[k]=slider
 
     with st.expander('impurity'):
-        imp_loc=st.select_slider('Impurity location',options=np.arange(0,params['N'],0.1),value=round(0.5*params['N'],1),on_change=restart_run_button)
+        # store the value so that its not reset after N is changed
+        if 'imp_loc' not in st.sesssion_state:
+            st.session_state.imp_loc=round(0.5*params['N'],1)
+        imp_loc=st.select_slider('Impurity location',options=np.arange(0,params['N'],0.1),value=st.session_state.imp_loc,on_change=restart_run_button)
+        st.session_state.imp_loc=imp_loc
         imp_stength=st.select_slider('Impurity strength',options=np.arange(0,1,0.1),value=0,format_func=lambda x: f'{x:.2f}',on_change=restart_run_button)
         params['impurity']=f'-{imp_stength} *exp(-(x-{imp_loc})**2 )/(1**2)'
 
     with st.expander('Driving'):
         driving_str=st.select_slider('Pulse strength',options=np.arange(-1., 1.1, 0.1),value=0.,format_func=lambda x: f'{x:.2f}',on_change=restart_run_button)
-        pulse_max=st.select_slider('Pulse time',options=np.arange(0,params['t_max'],params['t_max']*0.1),value=0.,format_func=lambda x: f'{x:.2f}',on_change=restart_run_button)
+        # store the value so that its not reset after t_max is changed
+        if 'pulse_max' not in st.session_state:
+            st.session_state.pulse_max=0.
+        pulse_max=st.select_slider('Pulse time',options=np.arange(0,params['t_max'],0.1),
+            value=round(st.session_state.pulse_max,1),format_func=lambda x: f'{x:.2f}',on_change=restart_run_button)
+        st.session_state.pulse_max=pulse_max 
+
         params['driving']=f'{driving_str}*exp(-0.5*(t- {pulse_max})**2)*cos(2.*(t - {pulse_max}))'
         
         add_Joule=st.toggle('Joule heating',value=False,on_change=restart_run_button)
